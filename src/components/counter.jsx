45ebf2b1a563a8f8e0aff7b26ee1eee2ed7abe68
Diwakar Gupta <goodboydiwaker@gmail.com>
2023-07-13 14:38:06 +0530

    Added dynamic rendering and functionaliy to display image and tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,37 @@ import React, { Component } from "react";
 class Counter extends Component {
 	state = {
 		count: 0,
+		tags: ["tag1", "tag2", "tag3"],
 	};
 	render() {
 		return (
-			<React.Fragment>
-				<span>{this.state.count}</span> <button>Increment</button>
-			</React.Fragment>
+			<div>
+				<span className={this.getBadgeClass()}>{this.formatCount()}</span>{" "}
+				<button className="btn btn-secondary btn-sm">Increment</button>
+				<ul>{this.formatTags()}</ul>
+			</div>
+		);
+	}
+
+	getBadgeClass() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
+	}
+
+	formatCount() {
+		const { count } = this.state;
+
+		return count === 0 ? "Zero" : count;
+	}
+
+	formatTags() {
+		const { tags } = this.state;
+		return tags.length === 0 ? (
+			<p>"There no tags"</p>
+		) : (
+			this.state.tags.map((tag) => <li key={tag}>{tag}</li>)
 		);
 	}
 }
-
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Counter />
